3a9f7b82d2e4df3395a2f745d854d7bcabb40f62
sharmeenasifhussainsiddiqui <72220625+sharmeenasifhussainsiddiqui@users.noreply.github.com>
2021-02-08 17:48:47 +0500

    Add files via upload

---
+++ lcd.ino
@@ -0,0 +1,15 @@
+ #include <LiquidCrystal.h>
+ LiquidCrystal lcd (2,3,4,5,6,7); 
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16,2);
+  lcd.print("hello,World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(0,1);
+  lcd.print(millis()/1000);
+
+}
